710aafef2047b8e763dd6827aa99a9115c38cc27
Vitor Cardoso <vitorcnf64@gmail.com>
2022-02-28 19:52:19 -0300

    Updated appearance, board can be painted on now, added a header.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,16 @@
 const gridContainer = document.querySelector('#grid-container');
 
-let gridBoxes = [];
 for(let i = 0; i < 256; i++) {
-    gridBoxes[i] = document.createElement('div');
-    document.getElementById('grid-container').appendChild(gridBoxes[i]);
-    gridBoxes[i].className = 'grid-box';
+    let gridBoxes = document.createElement('div');
+    document.getElementById('grid-container').appendChild(gridBoxes);
+    gridBoxes.className = 'grid-box';
 }
+
+const gridBox = document.querySelectorAll('.grid-box');
+gridBox.forEach(gridBox => gridBox.addEventListener('mouseover', () => {
+    console.log('hey');
+    gridBox.setAttribute('style', 'background-color: black')
+}));
+
+// when you add the size selector thingy,
+// set the px size of the boxes to 500/boxNumber.
