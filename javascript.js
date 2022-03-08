206558685f062d41604ef62154ab59d4de7e78b2
Vitor Cardoso <vitorcnf64@gmail.com>
2022-03-08 19:52:06 -0300

    partially fixed reset button, it works only the 1st time. Box sizes are a bit off still. Added 2 new functions to aide in the resize process

--- javascript.js
+++ javascript.js
@@ -4,13 +4,16 @@ const resetBtn = document.querySelector('.reset-btn');
 // Creates a 16x16 grid of white squares inside the board.
 for(let i = 0; i < 256; i++) {
     let gridBox = document.createElement('div');
-    document.getElementById('grid-container').appendChild(gridBox);
+    gridContainer.appendChild(gridBox);
     gridBox.className = 'grid-box';
 }
 
 const gridBoxes = document.querySelectorAll('.grid-box');
 
-let currentPxSize = 500 / 16 - 2;
+let pxSize = 500 / 16 - 2;
+let previousNumberOfBoxes = 256;
+
+// Reset & Resize button.
 resetBtn.addEventListener('click', () => {
     gridBoxes.forEach(gridBoxes => gridBoxes.setAttribute('style', 'background-color: white;'));
 
@@ -19,30 +22,38 @@ resetBtn.addEventListener('click', () => {
         size = prompt('Set Size:');
         parseInt(size, 10);
     } while(size > 100);
-    console.log(size);
 
-    currentPxSize = 500 / size - 2;
-    resize(size);
+    pxSize = 500 / size - 2.001;
+
+    createNewGrid(size);
+    resize();
 });
 
-function resize(size) {
-    for(let i = 0; i < size * size; i++) {
+paint();
+
+// Functions:
+
+// Uses the user's input (size) to create a new grid with the desired size.
+function createNewGrid(size) {
+    for(let i = 0; i < size * size - previousNumberOfBoxes; i++) {
         let gridBox = document.createElement('div');
         document.getElementById('grid-container').appendChild(gridBox);
         gridBox.className = 'grid-box';
+        gridBox.setAttribute('style', `height: ${pxSize}px; width: ${pxSize}px;`);
     }
-    gridBoxes.forEach(gridBoxes => {
-        gridBoxes.setAttribute('style', `height: ${currentPxSize}px; width: ${currentPxSize}px;`);
+}
+
+// Resizes the grid's boxes to fit the container.
+function resize() {
+    gridBoxes.forEach(gridBox => {
+        gridBox.setAttribute('style', `height: ${pxSize}px; width: ${pxSize}px;`);
     });
 }
 
-// Turns the squares black when the cursor touches them.
-gridBoxes.forEach(gridBoxes => gridBoxes.addEventListener('mouseover', () => {
-    gridBoxes.setAttribute('style',
-    `background-color: black; border: solid 0.5px #070707;
-    height: ${currentPxSize}px; width: ${currentPxSize}px;`)
-}));
-
-// when you add the size selector thingy,
-// set the px size of t to 500/Number.
-// MAYBE add the remainder of 500/boxNumber to  container h & w.
+// Turns the squares black when the cursor hovers over them.
+function paint() {
+    gridBoxes.forEach(gridBoxes => gridBoxes.addEventListener('mouseover', () => {
+        gridBoxes.setAttribute('style',
+        `background-color: black; height: ${pxSize}px; width: ${pxSize}px;`)
+    }));
+}
